910a08ae91188b8c7df9d6226d0b397d2d2d4ced
Shiho(mia) <mia.can2021@gmail.com>
2022-04-29 08:53:30 -0700

    changed

--- src/components/PostPreview.tsx
+++ src/components/PostPreview.tsx
@@ -1,12 +1,8 @@
-import { ButtonBase } from "@mui/material";
-import React, { ReactElement, useState, useEffect } from "react";
-import { Post, Comment } from "../API";
+import React, { useState, useEffect } from "react";
+import { Post } from "../API";
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { API, Auth, Storage } from "aws-amplify";
-import { GRAPHQL_AUTH_MODE } from "@aws-amplify/api";
-import { useUser } from "../context/AuthContext";
-import { PostComment } from "./PostComment";
+import { Storage } from "aws-amplify";
 
 type Props = {
   post: Post;
@@ -31,12 +27,22 @@ export default function PostPreview({ post }: Props) {
   return (
     <div
       onClick={() => router.push(`/post/${post.id}`)}
-      className="w-full md:w-3/5 min-h-8 h-1/3 border flex flex-col items-center justify-center cursor-pointer py-5"
+      className="w-full md:w-3/5 min-h-8 h-1/3 border flex flex-col items-center justify-center cursor-pointer py-5 transition-transform hover:-translate-y-1"
     >
-      <h2 className="font-mono text-2xl">{post.title}</h2>
+      <div className="flex items-center gap-2 flex-col lg:flex-row justify-center">
+        <h2 className="font-mono text-2xl">{post.title}</h2>
+        <p className="text-sm">{new Date(post.updatedAt).toLocaleString()}</p>
+      </div>
+      <div className="flex gap-2 mb-3">
+        {post.tags.map((tag) => (
+          <div className="badge badge-primary" key={tag + post.id}>
+            {tag}
+          </div>
+        ))}
+      </div>
       {post.image && postImage && (
         <Image
-          src={`${postImage}`}
+          src={postImage}
           height={200}
           width={300}
           layout="intrinsic"
@@ -49,28 +55,19 @@ export default function PostPreview({ post }: Props) {
           ? post.contents.substr(0, 200) + "..."
           : post.contents}
       </p>
-      <div className="flex gap-2">
-        {post.tags.map((tag) => (
-          <div className="badge badge-primary" key={tag + post.id}>
-            {tag}
+      <div className="flex items-center gap-3 justify-end w-full mr-10">
+        <p className="font-mono font-bold">{post.owner}</p>
+        <div className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full overflow-hidden">
+            <Image
+              layout="fill"
+              alt="usericon"
+              className="w-10 rounded-full"
+              src={"/icon.png"}
+            />
           </div>
-        ))}
+        </div>
       </div>
-      {/* <svg
-        xmlns="http://www.w3.org/2000/svg"
-        className="h-6 w-6"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-        strokeWidth={2}
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-        />
-      </svg> */}
-      <p>{post.owner}</p>
     </div>
   );
 }
